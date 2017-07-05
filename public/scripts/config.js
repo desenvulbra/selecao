3e10f9ce60434cc8a4f934d008078e5a5c50214e
zieg <zieg@localhost.localdomain>
2017-07-05 15:51:42 -0300

    Refatorando o arquivo de configurações.

--- public/scripts/config.js
+++ public/scripts/config.js
@@ -1,23 +1,3 @@
-// config
-
-var app =  
-angular.module('app')
-  .config(
-    [        '$controllerProvider', '$compileProvider', '$filterProvider', '$provide', 'ngToastProvider',
-    function ($controllerProvider,   $compileProvider,   $filterProvider,   $provide,   ngToastProvider) {
-        
-        // lazy controller, directive and service
-        app.controller = $controllerProvider.register;
-        app.directive  = $compileProvider.directive;
-        app.filter     = $filterProvider.register;
-        app.factory    = $provide.factory;
-        app.service    = $provide.service;
-        app.constant   = $provide.constant;
-        app.value      = $provide.value;
-        //desativa o debug - aumentando o ganho de performance
-        //$compileProvider.debugInfoEnabled(false);
-    }
-  ])
-  .config(['$httpProvider', function($httpProvider) {
+var app = angular.module('app').config(['$httpProvider', function($httpProvider) {
     $httpProvider.interceptors.push('authInterceptor');
-  }]);
+}]);
