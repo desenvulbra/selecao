6546d1cf51e1d16683db35154133abf3b011478e
zieg <zieg@localhost.localdomain>
2017-07-05 14:14:21 -0300

    Configs gerais + Gancho para verificar se está logado.

---
+++ public/scripts/app.ctrl.js
@@ -0,0 +1,28 @@
+'use strict';
+angular.module('app')  
+    .controller('AppCtrl', ['$scope', '$window', '$document', '$location', '$rootScope', '$timeout',
+    function (               $scope,   $window,   $document,   $location,   $rootScope,   $timeout) {
+        // config
+        $scope.app = {
+            name: 'Ulbra',
+            version: '1.0.0'
+        } 
+
+        $scope.goBack = function () {
+            $window.history.back();
+        }
+
+    }
+]).run([ '$rootScope', '$state', '$location', 'cursosAPI', '$transitions', function run($rootScope, $state, $location, cursosAPI, $transitions) {
+    console.info('Anderson Bargas');
+
+    $transitions.onStart({ to: 'cursos' }, function(trans) {
+        var $state = trans.router.stateService;
+        return cursosAPI.getLogado().then(function(res){//Quando fizer um cold reload, verifica se está logado
+            var logado = res.logado;
+            if(logado){ return true; }
+            return $state.target('login');
+        });
+    });
+
+}]);
