c287da11fee2267b52380cba1395f1c87f338ef2
zieg <zieg@localhost.localdomain>
2017-07-06 18:05:22 -0300

    Coment[arios + rota de logout.

--- app/app.js
+++ app/app.js
@@ -1,12 +1,17 @@
-import angular from 'angular';
-import uirouter from 'angular-ui-router';
-import routing from './config';
-import start from './start';
+import angular from 'angular';                              // angular
+import uirouter from 'angular-ui-router';                   // router
+import routing from './config';                             // configurações do router
+import start from './start';                                // verificação cold reload
 
-import login from './modules/login';
-import cursos from './modules/cursos';
+import login from './modules/login';                        // módulo login
+import cadastro from './modules/cadastro';                  // módulo cadastro
+import cursos from './modules/cursos';                      // módulo cursos
+import sair from './modules/sair';                          // módulo sair (apenas rota)
 
 import 'bootstrap/dist/css/bootstrap.css';
 import './styles/style.css';
 
-angular.module('app', [uirouter, login, cursos]).config(routing).run(start);
+const modulos = [uirouter, login, cadastro, cursos, sair];  // módulos da nossa aplicação
+angular.module('app', modulos)                              // injeta os módulos
+.config(routing)                                            // configurações do router
+.run(start);                                                // verificação cold reload
