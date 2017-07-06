bd2f4be2344a4214c043f752b3542f0bad909f2e
zieg <zieg@localhost.localdomain>
2017-07-06 18:07:14 -0300

    Comentário,

--- app/start.js
+++ app/start.js
@@ -1,23 +1,22 @@
-import UlbraService from './services/ulbraService';
+import UlbraService from './services/ulbraService';         // serviço
 
-start.$inject = ['$transitions', 'UlbraService'];
+start.$inject = ['$transitions', 'UlbraService'];           // injeção
 
-export default function start($transitions, UlbraService) {
-    //Quando fizer um cold reload, verifica se está logado
-    $transitions.onStart({ to: 'cursos' }, function(trans) {
-        var $state = trans.router.stateService;
-        return UlbraService.getLogado().then(function(res){
-            var logado = res.data.logado;
-            if(logado){ return true; }
-            return $state.target('login');
+export default function start($transitions, UlbraService) { // verificação cold reload
+    $transitions.onStart({ to: 'cursos' }, function(trans) {// quando ir para cursos
+        var $state = trans.router.stateService;             // objeto para redirecionar
+        return UlbraService.getLogado().then(function(res){ // consulta o serviço
+            var logado = res.data.logado;                   // logado?
+            if(logado){ return true; }                      // mantêm a rota
+            return $state.target('login');                  // redireciona para login
         });
     });
-    $transitions.onStart({ to: 'login' }, function(trans) {
-        var $state = trans.router.stateService;
-        return UlbraService.getLogado().then(function(res){
-            var logado = res.data.logado;
-            if(logado){ return $state.target('cursos'); }
-            return true;
+    $transitions.onStart({ to: 'login' }, function(trans) { // quando ir para login
+        var $state = trans.router.stateService;             // objeto para redirecionar
+        return UlbraService.getLogado().then(function(res){ // consulta o serviço
+            var logado = res.data.logado;                   // logado?
+            if(logado){ return $state.target('cursos'); }   // redireciona para cursos
+            return true;                                    // mantêm a rota
         });
     });
 }
