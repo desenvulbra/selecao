2d5526ec559eafd51ae52f95ed683a6b6371f4c1
Matheus Wichman <matheus.wichman@gmail.com>
2017-08-10 15:12:41 -0300

    Gulp configurado

---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var cssmin = require('gulp-cssmin');
+var uglify = require('gulp-uglify');
+
+gulp.task('vendor-js', function() {
+  gulp.src([
+    './node_modules/angular/angular.js',
+    './node_modules/@uirouter/angularjs/release/angular-ui-router.js',
+    './node_modules/angular-animate/angular-animate.js',
+    './node_modules/angular-messages/angular-messages.js',
+    './node_modules/angular-sanitize/angular-sanitize.js',
+    './node_modules/angular-toastr/dist/angular-toastr.js',
+    './node_modules/angular-input-masks/releases/angular-input-masks-standalone.js'
+  ]).pipe(concat('vendor.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('public/js/'))
+});
+
+gulp.task('vendor-css', function() {
+  gulp.src([
+    './node_modules/angular-toastr/dist/angular-toastr.css'
+  ]).pipe(concat('vendor.css'))
+    .pipe(cssmin())
+    .pipe(gulp.dest('public/css/'));
+});
+
+gulp.task('scripts', function() {
+  return gulp.src('assets/js/**/*.js')
+    .pipe(concat('app.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('public/js/'));
+});
+
+gulp.task('compile', [
+  'vendor-js',
+  'vendor-css',
+  'scripts'
+]);
+
+gulp.task('watch', function() {
+  return gulp.watch(['assets/js/**/*.js'], [
+    'scripts' ,
+    'vendor-js', 
+    'vendor-css'
+  ]);
+});
+
+gulp.task('default', ['watch']);
